2b218cbd73b38380fab39c1d67b0b65301404af0
happycamper1129 <happycamper.sw@gmail.com>
2019-01-08 20:51:16 +0000

    Update @MathiasKandelborg as a contributor

--- example/__tests__/e2e/e2e.test.js
+++ example/__tests__/e2e/e2e.test.js
@@ -1,6 +1,5 @@
 /* eslint-env jest */
 const testLanguage = 'en'
-const testLanguageSecondary = 'de'
 
 describe('example project', () => {
   beforeAll(async () => {
@@ -14,35 +13,9 @@ describe('example project', () => {
     await expect(page).toMatch('A simple example')
   })
 
-  it('should navigate to the second page and back', async () => {
+  it('should navigate to the second page', async () => {
     await expect(page).toClick('a', { text: 'To second page' })
     await expect(page).toMatch('A second page')
-
-    await expect(page).toClick('a', { text: 'Back to home' })
-    await expect(page).toMatch('A simple example')
   })
 
-  describe(`changing to ${testLanguageSecondary} locale`, () => {
-    beforeAll(async () => {
-      await expect(page).toClick('button', { text: 'Change locale' })
-    })
-
-    it(`should display h1 in ${testLanguageSecondary} locale`, async () => {
-      await expect(page).toMatch('Ein einfaches Beispiel')
-    })
-
-    it('should navigate to the second page and back', async () => {
-      await expect(page).toClick('a', { text: 'Zur zweiten Seite' })
-      await expect(page).toMatch('Eine zweite Seite')
-
-      await expect(page).toClick('a', { text: 'Zurück zur Hauptseite' })
-      await expect(page).toMatch('Ein einfaches Beispiel')
-    })
-
-    it(`should switch back to ${testLanguage} locale`, async () => {
-      await expect(page).toClick('button', { text: 'Wechseln Locale' })
-
-      await expect(page).toMatch('A simple example')
-    })
-  })
 })
